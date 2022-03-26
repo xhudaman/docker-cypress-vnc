341cd17eb58412894ae1ddb7a915fa93f31b398a
xhudaman <edell.matthew@gmail.com>
2022-03-26 01:32:00 -0400

    Added tests to confirm default state

--- cypress-testing/cypress/integration/api.js
+++ cypress-testing/cypress/integration/api.js
@@ -1,8 +1,37 @@
 describe("tasks", () => {
+  it("has the navbar and its contents", () => {
+    cy.visit("/");
+    cy.contains("Todo App - Dockerize Me");
+    cy.contains(".btn-outline-light", "Add Task");
+  });
+
   it("returns default tasks", () => {
     cy.visit("/");
 
-    cy.contains("Todo App - Dockerize Me");
-    cy.contains(".btn-outline-light", "Add Task");
+    cy.contains(1)
+      .parent("tr")
+      .within(() => {
+        cy.get("td").contains(
+          ".btn-outline-secondary",
+          "Dockerize a React app"
+        );
+
+        cy.get("td").contains(".badge", "True");
+
+        cy.get("td").contains(".btn-outline-danger", "Del");
+      });
+
+    cy.contains(2)
+      .parent("tr")
+      .within(() => {
+        cy.get("td").contains(
+          ".btn-outline-secondary",
+          "Dockerize a Node/Express app"
+        );
+
+        cy.get("td").contains(".badge", "True");
+
+        cy.get("td").contains(".btn-outline-danger", "Del");
+      });
   });
 });
